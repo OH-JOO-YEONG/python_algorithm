3c3c5c917e1dc5636c84d64728aeecc8d7ee52ec
OH-JOO-YEONG <brb1111@naver.com>
2020-03-25 00:46:16 +0900

    inflearnAlgo 4-5 done

---
+++ InflearnPythonAlgorithm/Section4/4-5.py
@@ -0,0 +1,35 @@
+from collections import deque
+
+n, k = map(int, input().split())
+
+q = [i for i in range(1, n + 1)]
+
+q = deque(q)
+
+cnt = 0
+while len(q) != 1:
+    if cnt == k - 1:
+        q.popleft()
+        cnt = 0
+    else:
+        cnt += 1
+        q.append(q.popleft())
+print(q[0])
+
+"""
+정답지
+"""
+n, k = map(int, input().split())
+
+dq = list(range(1, n + 1))
+
+dq = deque(dq)
+
+while dq:
+    for _ in range(k-1):
+        cur = dq.popleft()
+        dq.append(cur)
+    dq.popleft()
+    if len(dq) == 1:
+        print(dq[0])
+        dq.popleft()
